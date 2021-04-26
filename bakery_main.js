790d20c1cb4ef3f12d11597f136b6928b709f809
koohyeon <oldandnow9@gmail.com>
2021-04-26 10:56:59 +0900

    JS: Countdown Timer

--- bakery_main.js
+++ bakery_main.js
@@ -1,28 +1,56 @@
 "use strict";
 
 
-const game = function() {
-    const bg = document.querySelector('.background');
-    const imgs = document.querySelectorAll('img');
+const bg = document.querySelector('.background');
+const imgs = document.querySelectorAll('img');
+const breads = document.querySelectorAll('.bread');
+const runjis = document.querySelectorAll('.runji');
+
     
-    const maxHeight = bg.offsetHeight / 3;
-    const maxWidth = 800;
+const maxHeight = bg.offsetHeight / 3;
+const maxWidth = 800;
 
-    function createImage () {
-        for (let i = 0; i < 10; i++ ) {
-            const x =  Math.floor( Math.random() * maxWidth );
-            const y =  Math.floor( Math.random() * maxHeight );
-            const r = [x, y];
+const maxImg = 17
 
-            imgs[i].classList.remove("hidden");
-            imgs[i].style.bottom = r[1] + 'px';
-            imgs[i].style.left = r[0] + 'px';
-        }
-    };
+function createImage () {
+    for (let i = 0; i < maxImg; i++ ) {
+        const x =  Math.floor( Math.random() * maxWidth );
+        const y =  Math.floor( Math.random() * maxHeight );
+        const r = [x, y];
 
-    createImage();
+        imgs[i].classList.remove("hidden");
+        imgs[i].style.bottom = r[1] + 'px';
+        imgs[i].style.left = r[0] + 'px';
+    }
 };
 
 
 
-window.addEventListener('load', game);
+const countTimer = document.querySelector('.count__timer');
+countTimer.textContent = 10;
+const countNumber = document.querySelector('.count__number');
+const countFrom = 10;
+let countSec = 0;
+
+let presentNumber = 0;
+const goalNumber = 10;
+
+
+function countCatch () {
+    countNumber.textContent = `${presentNumber} / ${goalNumber}`;
+}
+
+createImage();
+countCatch ()
+
+const countTime = window.setInterval(function() {
+
+    countTimer.textContent = countFrom - countSec;
+
+    countSec++;
+
+    if (countSec > 10) {
+        clearInterval(countTime);
+        alert("Time's out!");}
+
+} , 1000);
