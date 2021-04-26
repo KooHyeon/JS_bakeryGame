3de307ae09a598be40dcd05acdf006e8114cab92
koohyeon <oldandnow9@gmail.com>
2021-04-26 11:51:20 +0900

    JS: Play Button

--- bakery_main.js
+++ bakery_main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const playBtn = document.querySelector('.play__btn');
 
 const bg = document.querySelector('.background');
 const imgs = document.querySelectorAll('img');
@@ -10,7 +11,7 @@ const runjis = document.querySelectorAll('.runji');
 const maxHeight = bg.offsetHeight / 3;
 const maxWidth = 800;
 
-const maxImg = 17
+const maxImg = 16
 
 function createImage () {
     for (let i = 0; i < maxImg; i++ ) {
@@ -25,7 +26,6 @@ function createImage () {
 };
 
 
-
 const countTimer = document.querySelector('.count__timer');
 countTimer.textContent = 10;
 const countNumber = document.querySelector('.count__number');
@@ -34,23 +34,39 @@ let countSec = 0;
 
 let presentNumber = 0;
 const goalNumber = 10;
+countNumber.textContent = `${presentNumber} / ${goalNumber}`;
 
 
-function countCatch () {
-    countNumber.textContent = `${presentNumber} / ${goalNumber}`;
-}
+playBtn.addEventListener('click', () => {
+    createImage();
+    playBtn.classList.add('hidden');
+    countTimer.classList.remove('hidden');
+    countNumber.classList.remove('hidden');
 
-createImage();
-countCatch ()
+    const countTime = window.setInterval(function() {
+        countTimer.textContent = countFrom - countSec;
+    
+        countSec++;
+    
+        if (countSec > 10) {
+            clearInterval(countTime);
+            alert("Time's out!");}
+    
+    } , 1000);
+})
 
-const countTime = window.setInterval(function() {
-
-    countTimer.textContent = countFrom - countSec;
-
-    countSec++;
-
-    if (countSec > 10) {
+const imgContainer = document.querySelector('.img__container');
+imgContainer.addEventListener('click', (e) => {
+    if (e.target.className === 'runji' && e.target.className !== 'disable') {
+        alert("Lose!");
         clearInterval(countTime);
-        alert("Time's out!");}
-
-} , 1000);
+        e.target.classList.add('disable');
+        console.log(e.target.className);
+    } else if (e.target.className === 'bread' && e.target.className !== 'disable') {
+        presentNumber = presentNumber + 1;
+        e.target.classList.add('disable');
+    } else if (presentNumber == goalNumber) {
+        alert("WIN");
+    }
+    countNumber.textContent = `${presentNumber} / ${goalNumber}`;
+});
